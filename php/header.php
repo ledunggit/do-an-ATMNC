550a27165dfb56152a704bd2c5cc5bc5d0a022cd
Lê Dũng <ledung.itsme@gmail.com>
2021-05-26 23:19:29 +0700

    Đồ án ATMNC

---
+++ php/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NT132.L12.ATCL - Deploy a website using Docker</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="title">Hello!</div>
+        <div class="class-code">NT132.L12.ATCL - Deploy a website using Docker - Team 10</div>
+        <div class="content">
+            <div class="student">
+                <div class="student-name"></div>
+                <div class="student-id"></div>
+            </div>
+            <div class="student">
+                <div class="student-name"></div>
+                <div class="student-id"></div>
+            </div>
+            <div class="connect">
